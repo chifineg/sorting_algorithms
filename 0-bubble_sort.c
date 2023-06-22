6a1ccac4082193016e9defecc0a8278f34f7cf62
Chidinma <chifineg@yahoo.com>
2023-06-22 03:14:35 -0700

    added files for task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Function to swap two int in an array.
+ * @a: first int to swap.
+ * @b: second int to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - func to sort an array of int in ascending order.
+ * @array: Array of integers to sort.
+ * @size: size of the array.
+ *
+ * Description: Prints array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t x, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (x = 0; x < len - 1; x++)
+		{
+			if (array[x] > array[x + 1])
+			{
+				swap_ints(array + x, array + x + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
