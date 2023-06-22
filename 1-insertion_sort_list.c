1c3342fa68640a71a9df847f129551f72884f136
Chidinma <chifineg@yahoo.com>
2023-06-22 14:02:44 -0700

    added files for task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - func to swap two nodes
+ * 	in a listint_t doubly-linked list.
+ * @h: Points to head of the doubly-linked list.
+ * @n1: Points to the first node to swap.
+ * @n2: Second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Func to sort a doubly linked list of integers
+ *                       using insertion sort algorithm.
+ * @list: Points to head of a doubly-linked list of integers.
+ *
+ * Description: Prints list after each swap.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iter, *insert, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (iter = (*list)->next; iter != NULL; iter = temp)
+	{
+		temp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
